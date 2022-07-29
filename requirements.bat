111380cb8b8335cf56c891a5fa193d1e34a18167
ExtortedNextrix <110208771+ExtortedNextrix@users.noreply.github.com>
2022-07-28 20:27:32 -0500

    Libraries installer
    Install the required libraries if you haven't got them already...

--- requirements.bat
+++ requirements.bat
@@ -3,8 +3,10 @@ color 0B
 pip install pyautogui
 pip install keyboard
 pip install colorama
+pip install ctypes
 python3 -m pip install pyautogui
 python3 -m pip install keyboard
 python3 -m pip install colorama
+python3 -m pip install ctypes
 
 PAUSE
