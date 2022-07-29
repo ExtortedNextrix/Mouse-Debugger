7f28e7ddec3368b478cb6660804d4de9694f1f10
ExtortedNextrix <110208771+ExtortedNextrix@users.noreply.github.com>
2022-07-28 20:15:24 -0500

    Libraries installer
    Run if you don't have necessary libraries installed

---
+++ requirements.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0B
+pip install pyautogui
+pip install keyboard
+pip install colorama
+python3 -m pip install pyautogui
+python3 -m pip install keyboard
+python3 -m pip install colorama
+
+PAUSE
